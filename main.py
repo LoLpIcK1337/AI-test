96df27767f1bf8c5113b49736dc0834aa2c8014a
LoLpIcK1337 <93485441+LoLpIcK1337@users.noreply.github.com>
2023-10-20 23:09:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from transformers import LlamaForCausalLM, LlamaConfig
 import torch
-# Создаем конфигурацию для модели
-config = LlamaConfig(
-    vocab_size=4024,  # размер словаря
-    d_model=512,  # размерность векторов слов
-    num_attention_heads=8,  # количество голов внимания
-    num_layers=12,  # количество слоев в модели
-    eos_token_id=4023,  # ID токена конца предложения
-    pad_token_id=4023,  # ID токена паддинга
-)
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+# Модель
+class CalculatorModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(CalculatorModel, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
 
-# Создаем модель с этой конфигурацией
-model = LlamaForCausalLM(config)
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+    
+input_dim = 3
+hidden_dim = 64
+output_dim = 1
+# Загрузка модели
+model = CalculatorModel(input_dim, hidden_dim, output_dim)
+model.load_state_dict(torch.load('calculator_model.pth'))
+model.eval()  # Переводим модель в режим оценки
 
-# Сохраняем модель
-torch.save(model.state_dict(), 'D:\\AI-test\\model\\model.pth')
+# Использование модели
+data = np.array([[5, 10, 0]])  # Пример данных
+inputs = torch.from_numpy(data).float()
+outputs = model(inputs)
+print(outputs)
